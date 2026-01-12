c9bbfb924ec637b8b10aa4a5e43dfc4490fa047c
Lucie Stehelin de Taisne <veltzlucie@gmail.com>
2026-01-12 12:16:55 +0700

    feat(tunnel): add /confirm-email alias for email links

---
+++ app/confirm-email/page.tsx
@@ -0,0 +1,5 @@
+// Alias “propre” pour les emails : /confirm-email?token=...
+// (on réutilise l'implémentation existante qui appelle le Back Office)
+export { default } from "../public/leads/confirm/page";
+
+
